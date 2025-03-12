ba65940319a8bee72628874511de7c2b27838401
YamanJamshaid <127229667+YamanJamshaid@users.noreply.github.com>
2025-03-12 22:13:07 +0500

    Projects.jsx image path changes

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -8,7 +8,7 @@ export const Projects = () => {
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-emerald-500 to-cyan-500/80 bg-clip-text text-transparent text-center">Projects</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-cyan-500/30 hover:shadow-[0_2px_8px_rgba(20, 255, 200, 0.2)] transition">
-                            <img src="/images/Aspproject.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg mb-4" />
+                            <img src="/Portfolio/images/Aspproject.jpg" alt="Project 1" className="w-full h-48 object-cover rounded-lg mb-4" />
                             <h3 className="text-2xl font-bold mb-2">Star Security</h3>
                             <p className="text-gray-400 mb-4">A simple website with crud operations and Admin Dashboard where crud operations are perfomed.</p>
                             <div className="flex flex-wrap gap-2 mb-4">
@@ -23,7 +23,7 @@ export const Projects = () => {
                             </div>
                         </div>
                         <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-cyan-500/30 hover:shadow-[0_2px_8px_rgba(20, 255, 200, 0.2)] transition">
-                            <img src="/images/TJsolution.jpg" alt="Project 2" className="w-full h-48 object-cover rounded-lg mb-4" />
+                            <img src="/Portfolio/images/TJsolution.jpg" alt="Project 2" className="w-full h-48 object-cover rounded-lg mb-4" />
                             <h3 className="text-2xl font-bold mb-2">TJ Solutions</h3>
                             <p className="text-gray-400 mb-4">Developed HTML, CSS Website on the given template</p>
                             <div className="flex flex-wrap gap-2 mb-4">
@@ -39,7 +39,7 @@ export const Projects = () => {
                         </div>
                         
                         <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-cyan-500/30 hover:shadow-[0_2px_8px_rgba(20, 255, 200, 0.2)] transition">
-                            <img src="/images/myportfolio.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4" />
+                            <img src="/Portfolio/images/myportfolio.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4" />
                             <h3 className="text-2xl font-bold mb-2">My Portfolio</h3>
                             <p className="text-gray-400 mb-4">Create the My personal portfolio website using react and tailwindcss.</p>
                             <div className="flex flex-wrap gap-2 mb-4">
@@ -54,7 +54,7 @@ export const Projects = () => {
                             </div>
                         </div>
                         <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-cyan-500/30 hover:shadow-[0_2px_8px_rgba(20, 255, 200, 0.2)] transition">
-                            <img src="/images/.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4"/>
+                            <img src="/images/.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4" alt="Movie booking"/>
                             <h3 className="text-2xl font-bold mb-2">Movie Ticket Booking site</h3>
                             <p className="text-gray-400 mb-4">Created Movie Ticket Booking site using php and phpmyadmin.</p>
                             <div className="flex flex-wrap gap-2 mb-4">
@@ -69,7 +69,7 @@ export const Projects = () => {
                             </div>
                         </div>
                         <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-cyan-500/30 hover:shadow-[0_2px_8px_rgba(20, 255, 200, 0.2)] transition">
-                            <img src="/images/graocoffee.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4" />
+                            <img src="/Portfolio/images/graocoffee.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4" />
                             <h3 className="text-2xl font-bold mb-2">GraoCoffee</h3>
                             <p className="text-gray-400 mb-4">Create the UI of Graocoffee eccomerce store.</p>
                             <div className="flex flex-wrap gap-2 mb-4">
@@ -84,7 +84,7 @@ export const Projects = () => {
                             </div>
                         </div>
                         <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-cyan-500/30 hover:shadow-[0_2px_8px_rgba(20, 255, 200, 0.2)] transition">
-                            <img src="/images/Thegemstore.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4" />
+                            <img src="/Portfolio/images/Thegemstore.jpg" alt="Project 3" className="w-full h-48 object-cover rounded-lg mb-4" />
                             <h3 className="text-2xl font-bold mb-2">Thegemstore</h3>
                             <p className="text-gray-400 mb-4">Create the UI of Thegemstore eccomerce store.</p>
                             <div className="flex flex-wrap gap-2 mb-4">
